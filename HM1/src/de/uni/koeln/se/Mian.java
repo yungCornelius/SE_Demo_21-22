ff30e674e0b6cbe9b68e1804f53309e616a62c13
Mersedeh Sadeghi <mersedeh.sadeghi@mail.polimi.it>
2021-10-21 21:54:03 +0200

    HM1

---
+++ HM1/src/de/uni/koeln/se/Mian.java
@@ -0,0 +1,14 @@
+package de.uni.koeln.se;
+
+public class Mian {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		System.out.println("Go0o0dbye World!");
+		
+		System.out.println("wait, it is too soon to say goodbye :D!");
+
+
+	    }
+}
