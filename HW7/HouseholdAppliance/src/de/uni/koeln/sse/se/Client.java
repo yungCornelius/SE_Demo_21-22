b2bc673789a067660230b6fa3d0a4946f1afbe05
Mersedeh Sadeghi <mersedeh.sadeghi@mail.polimi.it>
2021-11-24 16:25:32 +0100

    HW7

---
+++ HW7/HouseholdAppliance/src/de/uni/koeln/sse/se/Client.java
@@ -0,0 +1,33 @@
+package de.uni.koeln.sse.se;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Client{
+	
+public static void main(String[] args) {
+		
+
+		//Initialise some household
+		List<HouseholdItem> householdItmes= new ArrayList<>();
+
+		
+		Electronic microwaveOven= new Electronic("Microwave Oven",37, 48, 27, 13, false);
+		Electronic tv= new Electronic("TV",122, 73, 15, 18, true);
+		
+		Glass wineGlass= new Glass("Wine glasses",33,28,23,1);
+		Glass coffeeTable= new Glass("Glass Coffee Table ",80,80,45,3);
+
+		
+		Furniture bed= new Furniture("Bed", "Metal", 205, 91, 94, 26);
+		Furniture cupboard= new Furniture("Bookshelf", "Wood", 60, 31, 190, 25);
+		
+		
+        List<HouseholdItem> namesList = Arrays.asList(microwaveOven,tv,wineGlass,coffeeTable,bed,cupboard );
+        householdItmes.addAll(namesList);
+		
+  
+}
+
+}
---
+++ HW7/README.md
@@ -0,0 +1,6 @@
+## HW3: Java Implementation of State Machin In Java
+#### This is the template project for implementing State Machine in Java for the Elevator example (refer to HW3 Task sheet). 
+#### Task: 
+ - Add the missing elements to complete the program and then run it! 
+ - The default current_floor is 5th; run the program once with the destination floor higher, lower, and equal with the 5th floor and assess the program's behavior! 
+##### Hint: print the state of the system whenever a transition happens, so the program will demonstrate moving of the elevator from a floor to another! :relieved:
